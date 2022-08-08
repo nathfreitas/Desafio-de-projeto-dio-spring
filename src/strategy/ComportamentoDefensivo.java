839b9c30e06f9339f063df1b5cdecb4b6daac658
nathfreitas <nataliafreitasdacruz@gmail.com>
2022-08-08 15:45:09 -0300

    Desafio de Projeto Java

---
+++ src/strategy/ComportamentoDefensivo.java
@@ -0,0 +1,13 @@
+package strategy;
+
+public class ComportamentoDefensivo implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Movendo-se defensivamente...");
+		
+	} 
+	
+}
+
+
